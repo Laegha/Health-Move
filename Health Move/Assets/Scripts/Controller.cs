c74ab8eaa54641fdf60ffa83734d2c950772cfca
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2024-06-03 17:31:27 -0300

    feat(Controller): Class that stores values about controllers

---
+++ Health Move/Assets/Scripts/Controller.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller
+{
+    public int accelX;
+    public int accelY;
+    public int accelZ;
+
+    public Vector3 accel;
+
+}
