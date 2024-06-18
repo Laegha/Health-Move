8faa5c7c77f21efdc20a59918bdb1032ce63a1f2
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2024-06-18 14:32:12 -0300

    feat(PointScoredReciever): OnScored and OnRecieved
    Base class which every reciever will inherit. It is intended for an inherited class to call GameManager.gm.currMinigameManager.OnScored/OnRecieved

---
+++ Health Move/Assets/Scripts/Game Logic/PointScoredReciever.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PointScoredReciever : MonoBehaviour
+{
+    public virtual void OnScored()
+    {
+
+    }
+    public virtual void OnRecieved()
+    {
+
+    }
+}
