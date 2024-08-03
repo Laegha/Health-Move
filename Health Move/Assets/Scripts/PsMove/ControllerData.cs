f9766350a3f4656f06063c1c681ad1d67a807f29
Laegha <franciscolarghero@gmail.com>
2024-08-03 00:24:26 -0300

    refactor(Controller): renamed to "ControllerData"

--- Health Move/Assets/Scripts/PsMove/ControllerData.cs
+++ Health Move/Assets/Scripts/PsMove/ControllerData.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller
+public class ControllerData
 {
     public Vector3 accel;
     public Vector3 position;
+    public Vector3 movement;
     public Vector3 gyro;
     public Quaternion orientation;
 
+    public Vector3 positionZero;
 }
