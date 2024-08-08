99bba14c6c36dd6888dbae88e748359316516adf
Laegha <franciscolarghero@gmail.com>
2024-08-07 22:47:18 -0300

    refactor(ControllersHandler): Separated responsibilities
    This script now only handles processing data from controllers independantly from the tracker, and no longer handles the behaviour of calibrating and storing them

--- Health Move/Assets/Scripts/PsMove/ControllersHandler.cs
+++ Health Move/Assets/Scripts/PsMove/ControllersHandler.cs
@@ -3,86 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using PsMoveAPI;
 using System.Linq;
-using System.Collections;
 
-public class ControllersHandler : MonoBehaviour
+public class ControllersHandler
 {
-    //List<IntPtr> _controllers = new List<IntPtr>();
-    Dictionary<IntPtr, ControllerData> _controllers = new Dictionary<IntPtr, ControllerData>();
-    [SerializeField] Color _leds;
-    Color _prevLeds;
-
     int _accelDecimals = 4;
-    [SerializeField]float _sensitivity = 0.1f;
+    ControllersManager controllersManager;
 
-    public Dictionary<IntPtr, ControllerData> Controllers {  get { return _controllers; } }
-
-    IntPtr _camera;
-
-    static ControllersHandler instance;
-    public static ControllersHandler controllersHandler { get { return instance; } }
-
-    private void Awake()
+    public ControllersHandler(ControllersManager controllersManager)
     {
-        if(instance != null)
-            Destroy(gameObject);
-
-        instance = this;
-        DontDestroyOnLoad(gameObject);
-
-        if (ControllerHelper.psmove_init(ControllerHelper.PSMove_Version.PSMOVE_CURRENT_VERSION) == ControllerHelper.PSMove_Bool.PSMove_False)
-        {
-            Debug.Log("Failed to initialize PSMoveAPI. Probably using a wrong version");
-            return;
-        }
+        this.controllersManager = controllersManager;
     }
-
-    void Calibrate()
+    public void Update()
     {
-        print("Calibrating..."); 
-        int connectedControllers = ControllerHelper.psmove_count_connected();
-
-        for (int i = 0; i < connectedControllers; i++)
-        {
-            _controllers.Add(ControllerHelper.psmove_connect_by_id(i), new ControllerData());
-        }
-
-        _camera = ControllerHelper.psmove_tracker_new();
-        ControllerHelper.psmove_tracker_enable_deinterlace(_camera, true);
-
-        foreach (var controller in _controllers)
-        {
-            ControllerHelper.psmove_enable_orientation(controller.Key, true);
-            int enableError = 1;
-            enableError = ControllerHelper.psmove_tracker_enable(_camera, controller.Key);
-            print("Enabled: " + enableError);
-            //assignedController = controller.Key;
-            //StartCoroutine(Rainbow());
-            //ControllerHelper.psmove_set_leds(controller, 255, 255, 255);
-        }
-
-    }
-
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.C))
-            Calibrate();
-
-        if (_controllers.Count <= 0)
-            return;
-
-        ControllerHelper.psmove_tracker_update_image(_camera);
-
-        foreach (var controller in _controllers)
+        foreach (var controller in controllersManager.Controllers)
         {
             if (ControllerHelper.psmove_poll(controller.Key) == 0)
                 continue;
-
-            //if (_leds != _prevLeds)
-            //{
-            //    SetLeds(controller.Key, (byte)(_leds.r * 255), (byte)(_leds.g * 255), (byte)(_leds.b * 255));
-            //    _prevLeds = _leds;
-            //}
 
             #region Setting Accel
             //ControllerHelper.psmove_get_accelerometer(controller, ref x, ref y, ref z);
@@ -143,40 +79,8 @@ public class ControllersHandler : MonoBehaviour
 
             #endregion
 
-            if(ControllerHelper.psmove_tracker_update(_camera, controller.Key) == 0)
-            {
-                print("Update failed");
-                return;
-            }
-            print("Update succesfull");
-
-            #region Position Tracking
-            float posX = 0;
-            float posY = 0;
-            float radius = 0;
-
-            ControllerHelper.psmove_tracker_get_position(_camera, controller.Key, ref posX, ref posY, ref radius);
-            float posZ = ControllerHelper.psmove_tracker_distance_from_radius(_camera, radius);
-            posZ = TruncateDecimals(0, posZ);
-
-            Vector3 newControllerPosition = new Vector3(posX, posY, posZ);
-
-            controller.Value.movement = newControllerPosition - controller.Value.position;
-            controller.Value.position = newControllerPosition;
-
-            #endregion
-
-
-            //ControllerHelper.psmove_get_accelerometer_frame(controller, 1, ref x, ref y, ref z);
-
-
-            float xAccel = controller.Value.accel.x;
-
-            //if (xAccel > 0.1)
-                //Debug.Log(controller + " has accel: " + xAccel);
         }
 
-        GameManager.gm.ControllersUpdated();
     }
 
     float MapValue(float input)
@@ -194,7 +98,7 @@ public class ControllersHandler : MonoBehaviour
 
     public IntPtr GetControllerByIndex(int index)
     {
-        List<IntPtr> list = _controllers.Keys.ToList();
+        List<IntPtr> list = controllersManager.Controllers.Keys.ToList();
         
         return index < list.Count ? list[index] : (IntPtr)0;
     }
@@ -235,9 +139,5 @@ public class ControllersHandler : MonoBehaviour
     //        SetLeds(assignedController, red, green, blue);
     //    }
     //}
-    private void OnDestroy()
-    {
-        print("Destroyed");
-        ControllerHelper.psmove_tracker_free(_camera);
-    }
+
 }
