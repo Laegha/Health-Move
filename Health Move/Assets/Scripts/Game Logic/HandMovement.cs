59e916393893f54e055fef514bfdf2d5f7bc24ce
Laegha <franciscolarghero@gmail.com>
2024-08-07 22:42:01 -0300

    feat(HandMovement): Hand speed
    Serialized float to control the speed of each hand separately

--- Health Move/Assets/Scripts/Game Logic/HandMovement.cs
+++ Health Move/Assets/Scripts/Game Logic/HandMovement.cs
@@ -6,21 +6,23 @@ public class HandMovement : MonoBehaviour
 {
     IntPtr _assignedController;
 
-    public IntPtr AssignedController { get { return _assignedController; } set { _assignedController = value; } }
-
     static readonly float pixelToUnit = 0.005f;
     static readonly float cmToPixel = 200f;
+
+    [SerializeField] float handSpeed;
+
+    public IntPtr AssignedController { get { return _assignedController; } set { _assignedController = value; } }
 
     public void MovementUpdate()
     {
         if(_assignedController == IntPtr.Zero) 
-            _assignedController = ControllersHandler.controllersHandler.Controllers.Keys.ToArray()[0];
+            _assignedController = ControllersManager.controllersManager.Controllers.Keys.ToArray()[0];
 
-        Vector3 position = ControllersHandler.controllersHandler.Controllers[AssignedController].position;
-        Vector3 pixelMovement = ControllersHandler.controllersHandler.Controllers[AssignedController].movement;
+        Vector3 position = ControllersManager.controllersManager.Controllers[AssignedController].position;
+        Vector3 pixelMovement = ControllersManager.controllersManager.Controllers[AssignedController].movement;
         pixelMovement = new Vector3(pixelMovement.x * position.z * pixelToUnit, pixelMovement.y * position.z * pixelToUnit, pixelMovement.z * cmToPixel * pixelToUnit);
         print(pixelMovement);
-        Vector3 processedMovement = pixelMovement * Time.deltaTime /** sensitivity */;
+        Vector3 processedMovement = pixelMovement * Time.deltaTime * handSpeed /** sensitivity */;
 
         //print(position.z);
 
