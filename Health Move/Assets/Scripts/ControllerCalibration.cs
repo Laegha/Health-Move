9de6dd3fb5bd3367740ec36d0561f92478df3f13
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2024-08-12 17:25:19 -0300

    feat(ControllerCalibration): Disconnect existent controllers when starting calibration

--- Health Move/Assets/Scripts/ControllerCalibration.cs
+++ Health Move/Assets/Scripts/ControllerCalibration.cs
@@ -20,9 +20,17 @@ public class ControllerCalibration : MonoBehaviour
 
     void StartCalibration()
     {
+        foreach(var controller in ControllersManager.controllersManager.Controllers) //disconnect all controllers from hardware
+        {
+            ControllerHelper.psmove_disconnect(controller.Key);
+            ControllerHelper.psmove_tracker_disable(ControllersManager.controllersManager.Camera, controller.Key);
+        }
+
+        ControllersManager.controllersManager.Controllers.Clear(); //clear controllers dictionary
+
         int connectedControllers = ControllerHelper.psmove_count_connected();
 
-        for (int i = 0; i < connectedControllers; i++)
+        for (int i = 0; i < connectedControllers; i++) //connect all available controllers without camera
         {
             ControllersManager.controllersManager.Controllers.Add(ControllerHelper.psmove_connect_by_id(i), new ControllerData());
         }
@@ -31,27 +39,32 @@ public class ControllerCalibration : MonoBehaviour
         bool stillCalibrating = true;
         List<IntPtr> calibratedControllers = new List<IntPtr>();
 
-        while(stillCalibrating)
+        while(stillCalibrating)//this loop calibrates with camera all controllers willing to connect
         {
             foreach(var controller in ControllersManager.controllersManager.Controllers)
             {
-                if(controller.Value.pressedButtons == calibrationButton)
+                if(controller.Value.pressedButtons == calibrationButton)//if willing to connect, call CalibrateController and keep track of it
                 {
                     ControllersManager.controllersManager.CalibrateController(controller.Key);
                     calibratedControllers.Add(controller.Key);
                 }
                 
-                if(controller.Value.pressedButtons == endCalibrationButton && calibratedControllers.Contains(controller.Key))
+                if(controller.Value.pressedButtons == endCalibrationButton && calibratedControllers.Contains(controller.Key))//if connection was ended, end loop
                     stillCalibrating = false; 
 
             }
 
         }
 
-        foreach(var controller in ControllersManager.controllersManager.Controllers)
+        foreach(var controller in ControllersManager.controllersManager.Controllers)//remove from dictionary all controllers that weren't enabled with camera
         {
             if(!calibratedControllers.Contains(controller.Key))
+            {
+                ControllerHelper.psmove_disconnect(controller.Key);
                 ControllersManager.controllersManager.Controllers.Remove(controller.Key);
+            }
         }
+
+        StartCoroutine(ControllersManager.controllersManager.UpdateTracker());
     }
 }
