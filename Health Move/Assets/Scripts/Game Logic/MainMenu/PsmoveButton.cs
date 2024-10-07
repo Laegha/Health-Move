b6b60f1670e70fae99d8282b4e15ca9a088a7fa1
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2024-10-07 09:55:10 -0300

    refactor(PsmoveButton): Triggered by time hovering instead of button pressed

--- Health Move/Assets/Scripts/Game Logic/MainMenu/PsmoveButton.cs
+++ Health Move/Assets/Scripts/Game Logic/MainMenu/PsmoveButton.cs
@@ -2,6 +2,7 @@ using PsMoveAPI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 
 public class PsmoveButton : MonoBehaviour
@@ -9,6 +10,10 @@ public class PsmoveButton : MonoBehaviour
     [HideInInspector] public bool isInteractable = true;
 
     [SerializeField] UnityEvent onInteractedEvents;
+
+    float fillAmount = 0;
+
+    [SerializeField] float speed;
 
     private void OnTriggerStay(Collider other)
     {
@@ -20,9 +25,26 @@ public class PsmoveButton : MonoBehaviour
         if(playerCollisionIdentifier == null)
             return;
 
-        if(playerCollisionIdentifier.PlayerIdentifier.ControllerData.pressedButtons != ((ControllerHelper.PSMoveButton.Up - ControllerHelper.PSMoveButton.Up) | ControllerHelper.PSMoveButton.Trigger))
+        fillAmount += Time.deltaTime * speed;
+        other.GetComponentInChildren<Image>().fillAmount = fillAmount;
+
+        if(fillAmount >= 1)
         {
             onInteractedEvents.Invoke();
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!isInteractable)
+            return;
+
+        PlayerCollisionIdentifier playerCollisionIdentifier = other.GetComponent<PlayerCollisionIdentifier>();
+
+        if (playerCollisionIdentifier == null)
+            return;
+
+        fillAmount = 0;
+        other.GetComponentInChildren<Image>().fillAmount = fillAmount;
     }
 }
