6aecb081d7ac28ddf92fc9fe0efbfbdfbb281519
Laegha <franciscolarghero@gmail.com>
2024-10-08 21:04:30 -0300

    feat(BochasMinigame): MinigameManager and empty scripts for bochas and bochin

---
+++ Health Move/Assets/Scripts/Game Logic/Minigames/BochasMinigameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BochasMinigameManager : MinigameManager
+{
+    Transform _bochin;
+
+    int _player1Points;
+    int _player2Points;
+
+    public Transform Bochin {  get { return _bochin; } set { _bochin = value; }
+    public void BochinStopped()
+    {
+
+    }
+}
