f944b6111aaec44dc80a0904522fbd8c24d7d067
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2024-10-14 10:39:54 -0300

    feat(EndTurnWhenStopped): Added checking bool to see if it was already activated
    Also refactored form velocity <= 0 to <= 0.2. fixed a bug where it continued to work after destroyment with yield break

--- Health Move/Assets/Scripts/Game Logic/Minigames/General/EndTurnWhenStopped.cs
+++ Health Move/Assets/Scripts/Game Logic/Minigames/General/EndTurnWhenStopped.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EndTurnWhenStopped : MonoBehaviour
 {
     Rigidbody rb;
+    bool checking = false;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();    
@@ -12,14 +13,21 @@ public class EndTurnWhenStopped : MonoBehaviour
 
     public IEnumerator CheckSpeed()
     {
+        if (checking)
+            yield break;
+
+        checking = true;
+        
         while(true)
         {
             yield return null;
-
-            if (rb.velocity.magnitude <= 0)
+            if(rb == null)
+                yield break;
+            if (rb.velocity.magnitude <= 0.2f)
             {
                 GameManager.gm.CurrMinigameManager.OnTurnEnded();
                 Destroy(this);
+                yield break;
             }
         }
     }
