348585f0ab9e7eee8bb4027311b46f08adb4ec5d
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2024-11-04 10:26:41 -0300

    fix(CalibrateOnStart): No double screen

--- Health Move/Assets/CalibrateOnStart.cs
+++ Health Move/Assets/CalibrateOnStart.cs
@@ -6,35 +6,16 @@ using UnityEngine;
 public class CalibrateOnStart : MonoBehaviour
 {
     static bool started;
-    [SerializeField] GameObject screenPrefab;
-    GameObject instantiatedScreen;
 
     void Start()
     {
         if(started)
         {
-            instantiatedScreen = Instantiate(screenPrefab, GameObject.Find("Canvas").transform);
-            instantiatedScreen.SetActive(true);
             StartCoroutine(ControllerCalibration.controllerCalibration.StartCalibration(() =>
             {
                 GameManager.gm.ResetHands();
             }));
-            StartCoroutine(WaitForInput());
         }
         started = true;
-    }
-
-    IEnumerator WaitForInput()
-    {
-        while(true)
-        {
-            if (ControllersManager.controllersManager.Controller.Value.pressedButtons != ((ControllerHelper.PSMoveButton.Up - ControllerHelper.PSMoveButton.Up) | ControllerHelper.PSMoveButton.Trigger) && ControllersManager.controllersManager.Controller.Value.pressedButtons != ControllersManager.controllersManager.Controller.Value.prevPressedButtons)
-            {
-                Destroy(instantiatedScreen);
-                yield break;
-
-            }
-            yield return null;
-        }
     }
 }
